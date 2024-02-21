0609c8ae3d3a167989d11b5decbd4c50745736d9
enesharman <enesharman45@gmail.com>
2024-02-21 22:12:18 +0300

    initial commit

---
+++ src/main/java/com/netmera/hooksender/HookSenderApplication.java
@@ -0,0 +1,13 @@
+package com.netmera.hooksender;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HookSenderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HookSenderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/netmera/hooksender/HookSenderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.netmera.hooksender;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HookSenderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
